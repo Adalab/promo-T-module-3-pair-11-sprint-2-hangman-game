99d6ce3dad095a72059d80a849b0743909debaee
raquelgarciat <raquelgarciat@gmail.com>
2023-06-27 11:08:36 +0200

    migrate project

--- src/components/App.js
+++ src/components/App.js
@@ -1,105 +1,137 @@
-// Fichero src/components/App.js
-import '../styles/App.css';
-import '../styles/_reset.scss';
-import '../styles/_loading.scss';
-import '../styles/_letters.scss';
-import '../styles/_instructions.scss';
-import '../styles/_header.scss';
-import '../styles/_form.scss';
-import '../styles/_footer.scss';
-import '../styles/_dummy.scss';
-import '../styles/_app.scss';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+// api
+import getWordFromApi from '../services/api';
+// styles
+import '../styles/App.scss';
+import '../styles/Dummy.scss';
+import '../styles/Letters.scss';
+import '../styles/Form.scss';
+import '../styles/Header.scss';
 
 function App() {
-  let [numberOfErrors, setErrors] = useState(0);
-  const handleClick = (ev) => {
+  const [word, setWord] = useState('');
+  const [userLetters, setUserLetters] = useState([]);
+  const [lastLetter, setLastLetter] = useState('');
+
+  useEffect(() => {
+    getWordFromApi().then((word) => {
+      setWord(word);
+    });
+  }, []);
+
+  // events
+
+  const handleKeyDown = (ev) => {
+    // Sabrías decir para qué es esta línea
+    ev.target.setSelectionRange(0, 1);
+  };
+
+  const handleChange = (ev) => {
+    let re = /^[a-zA-ZñÑá-úÁ-Ú´]$/; //add regular pattern 
+    if (re.test(ev.target.value) || ev.target.value === '') {
+      handleLastLetter(ev.target.value);
+    }
+  };
+
+  const handleSubmit = (ev) => {
     ev.preventDefault();
-    setErrors (numberOfErrors + 1);
-    console.log (numberOfErrors);
-  }
+  };
 
-  let [lastLetter, setLastLetter] = useState('');
-  const handleInputChange = (event) => {
-    event.preventDefault();
-    const inputText = event.target.value;
-    const lastCharacter = inputText.charAt(inputText.length - 1);
-    /*if (esLetraValida(lastCharacter)) {
-      setLastLetter(lastCharacter);
-    } else {
-      setErrors(numberOfErrors + 1);
-    }*/
+  const getNumberOfErrors = () => {
+    const errorLetters = userLetters.filter(
+      (letter) => word.includes(letter) === false
+    );
+    return errorLetters.length;
+  };
 
-    /*const esLetraValida = (letra) => {
-    const alfabeto_espanol = "abcdefghijklmnñopqrstuvwxyzáéíóúü";
-    return alfabeto_espanol.includes(letra.toLowerCase());
-    console.log (esLetraValida);*/
+  const renderSolutionLetters = () => {
+    const wordLetters = word.split('');
+    return wordLetters.map((letter, index) => {
+      const exists = userLetters.includes(letter.toLocaleLowerCase());
+      return (
+        <li key={index} className='letter'>
+          {exists ? letter : ''}
+        </li>
+      );
+    });
+  };
+
+  const renderErrorLetters = () => {
+    const errorLetters = userLetters.filter(
+      (letter) =>
+        word.toLocaleLowerCase().includes(letter.toLocaleLowerCase()) === false
+    );
+    return errorLetters.map((letter, index) => {
+      return (
+        <li key={index} className='letter'>
+          {letter}
+        </li>
+      );
+    });
+  };
+
+  const handleLastLetter = (value) => {
+    value = value.toLocaleLowerCase();
+    setLastLetter(value);
+
+    if (!userLetters.includes(value)) {
+      userLetters.push(value);
+      setUserLetters([...userLetters]);
+    }
   };
 
   return (
-    <div className="page">
+    <div className='page'>
       <header>
-        <h1 className="header__title">Juego del ahorcado</h1>
+        <h1 className='header__title'>Juego del ahorcado</h1>
       </header>
-      <main className="main">
+      <main className='main'>
         <section>
-          <div className="solution">
-            <h2 className="title">Solución:</h2>
-            <ul className="letters">
-              <li className="letter">k</li>
-              <li className="letter">a</li>
-              <li className="letter"></li>
-              <li className="letter">a</li>
-              <li className="letter">k</li>
-              <li className="letter">r</li>
-              <li className="letter"></li>
-              <li className="letter">k</li>
-              <li className="letter">e</li>
-              <li className="letter">r</li>
-            </ul>
+          <div className='solution'>
+            <h2 className='title'>Solución:</h2>
+            <ul className='letters'>{renderSolutionLetters()}</ul>
           </div>
-          <div className="error">
-            <h2 className="title">Letras falladas:</h2>
-            <ul className="letters">
-              <li className="letter">f</li>
-              <li className="letter">q</li>
-              <li className="letter">h</li>
-              <li className="letter">p</li>
-              <li className="letter">x</li>
-            </ul>
+          <div className='error'>
+            <h2 className='title'>Letras falladas:</h2>
+            <ul className='letters'>{renderErrorLetters()}</ul>
           </div>
-          <form className="form">
-            <label className="title" htmlFor="last-letter">Escribe una letra:</label>
+          <form className='form' onSubmit={handleSubmit}>
+            <label className='title' htmlFor='last-letter'>
+              Escribe una letra:
+            </label>
             <input
-              autoComplete="off"
-              className="form__input"
-              maxLength="1"
-              type="text"
-              name="last-letter"
-              id="last-letter"
+              autoFocus
+              autoComplete='off'
+              className='form__input'
+              maxLength='1'
+              type='text'
+              name='last-letter'
+              id='last-letter'
               value={lastLetter}
-              onChange={handleInputChange}
+              onKeyDown={handleKeyDown}
+              onChange={handleChange}
             />
-            <input type='submit' value='Incrementar' onClick = {handleClick}></input>
           </form>
         </section>
-        <section className={`dummy error-${numberOfErrors}`}>
-          <span className="error-13 eye"></span>
-          <span className="error-12 eye"></span>
-          <span className="error-11 line"></span>
-          <span className="error-10 line"></span>
-          <span className="error-9 line"></span>
-          <span className="error-8 line"></span>
-          <span className="error-7 line"></span>
-          <span className="error-6 head"></span>
-          <span className="error-5 line"></span>
-          <span className="error-4 line"></span>
-          <span className="error-3 line"></span>
-          <span className="error-2 line"></span>
-          <span className="error-1 line"></span>
+        <section className={`dummy error-${getNumberOfErrors()}`}>
+          <span className='error-13 eye'></span>
+          <span className='error-12 eye'></span>
+          <span className='error-11 line'></span>
+          <span className='error-10 line'></span>
+          <span className='error-9  line'></span>
+          <span className='error-8  line'></span>
+          <span className='error-7  line'></span>
+          <span className='error-6  head'></span>
+          <span className='error-5  line'></span>
+          <span className='error-4  line'></span>
+          <span className='error-3  line'></span>
+          <span className='error-2  line'></span>
+          <span className='error-1  line'></span>
         </section>
       </main>
     </div>
   );
 }
+
 export default App;
