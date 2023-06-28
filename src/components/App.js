b6151105f7275cfc4bf2cb29121a062686a7c097
“AndreaFerreiro” <andreafga28@gmail.com>
2023-06-28 11:25:06 +0200

    finish components

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,13 @@
 import { useEffect, useState } from 'react';
 import Header from './Header';
 import Dummy from './Dummy';
+import SolutionLetters from './SolutionLetters';
+import ErrorLetters from './ErrorLetters';
 // api
 import getWordFromApi from '../services/api';
 // styles
 import '../styles/App.scss';
-import '../styles/Dummy.scss';
-import '../styles/Letters.scss';
 import '../styles/Form.scss';
-import '../styles/Header.scss';
 
 function App() {
   const [word, setWord] = useState('');
@@ -39,39 +38,7 @@ function App() {
     ev.preventDefault();
   };
 
-  const getNumberOfErrors = () => {
-    const errorLetters = userLetters.filter(
-      (letter) => word.includes(letter) === false
-    );
-    return errorLetters.length;
-  };
-
-  const renderSolutionLetters = () => {
-    const wordLetters = word.split('');
-    return wordLetters.map((letter, index) => {
-      const exists = userLetters.includes(letter.toLocaleLowerCase());
-      return (
-        <li key={index} className='letter'>
-          {exists ? letter : ''}
-        </li>
-      );
-    });
-  };
-
-  const renderErrorLetters = () => {
-    const errorLetters = userLetters.filter(
-      (letter) =>
-        word.toLocaleLowerCase().includes(letter.toLocaleLowerCase()) === false
-    );
-    return errorLetters.map((letter, index) => {
-      return (
-        <li key={index} className='letter'>
-          {letter}
-        </li>
-      );
-    });
-  };
-
+  
   const handleLastLetter = (value) => {
     value = value.toLocaleLowerCase();
     setLastLetter(value);
@@ -81,20 +48,24 @@ function App() {
       setUserLetters([...userLetters]);
     }
   };
-
+  const getNumberOfErrors = () => {
+    const errorLetters = userLetters.filter(
+      (letter) => word.includes(letter) === false
+    );
+    let numberOfErrors = errorLetters.length
+    return numberOfErrors;
+  };
   return (
     <div className='page'>
       <Header />
       <main className='main'>
         <section>
-          <div className='solution'>
-            <h2 className='title'>Solución:</h2>
-            <ul className='letters'>{renderSolutionLetters()}</ul>
-          </div>
-          <div className='error'>
-            <h2 className='title'>Letras falladas:</h2>
-            <ul className='letters'>{renderErrorLetters()}</ul>
-          </div>
+          <SolutionLetters
+          word={word}
+          userLetters={userLetters}/>
+          <ErrorLetters
+          word={word}
+          userLetters={userLetters}/>
           <form className='form' onSubmit={handleSubmit}>
             <label className='title' htmlFor='last-letter'>
               Escribe una letra:
@@ -113,7 +84,8 @@ function App() {
             />
           </form>
         </section>
-        <Dummy />
+        <Dummy 
+        numberOfErrors={getNumberOfErrors()}/>
       </main>
     </div>
   );
